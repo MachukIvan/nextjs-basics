a71e6ae03081df33ab1f806d7840b5e771617bce
Ivan <machuk.ivan@yandex.by>
2019-06-12 16:50:16 +0300

    Added getInitialProps

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import User from '../../components/User';
+import User from "../../components/User";
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
     <div>
         <h1>The Auth Index Page</h1>
         <User name="Ivan" age={24} />
     </div>
 );
+
+authIndexPage.getInitialProps = context => {
+    console.log(context);
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ appName: "Super App (Auth)" });
+        }, 1000);
+    });
+    return promise;
+};
 
 export default authIndexPage;
